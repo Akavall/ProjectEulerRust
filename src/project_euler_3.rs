65ee9897242a1a9ddc00b2083d2d51444345436b
Kirill <Akavall@gmail.com>
2016-01-19 19:37:52 -0600

    changed problem 3 to be solved via functional approach

--- src/project_euler_3.rs
+++ src/project_euler_3.rs
@@ -3,11 +3,6 @@ use utilities::prime_utilities::is_prime;
 pub fn project_euler_3(my_num: i64) -> i64 {
     let limit = (my_num as f64).sqrt() as i64 + 1;
     
-    let mut largest: i64 = 0;
-    for i in 0..limit {
-        if is_prime(i) && my_num % i == 0 {
-            largest = i;
-        }
-    }
+    let largest = (0..limit).filter(|&x| is_prime(x) && my_num % x == 0).max().unwrap();
     largest
 }
