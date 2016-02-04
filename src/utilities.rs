85cca6df69d1fec64c4d82e2143ab63b858a2fd1
Kirill <Akavall@gmail.com>
2016-02-03 22:03:34 -0600

    added is_palindrome

--- src/utilities.rs
+++ src/utilities.rs
@@ -17,14 +17,42 @@ pub mod prime_utilities {
     }
 
     #[test]
-    fn test_35() {
+    fn test_35_prime() {
         assert_eq!(is_prime(35), false);
     }
 
     #[test]
-    fn test_first_30() {
+    fn test_first_30_prime() {
         let result: Vec<i64> = (1..30).filter(|&x| is_prime(x) == true).collect();
         let expected: Vec<i64> = vec![2,3,5,7,11,13,17,19,23,29];
         assert_eq!(result, expected);
     }
+
+    pub fn is_palindrome(mut a: i64) -> bool {
+        let mut my_vec = vec![];
+        while a > 0{
+            my_vec.push(a % 10);
+            a = a / 10;
+        }
+        let mut rev_vec = my_vec.to_owned();
+        rev_vec.reverse();
+        let my_bool = rev_vec == my_vec;
+        return my_bool;
+    }
+
+    #[test]
+    fn test_0_is_palindrome() {
+        assert_eq!(is_palindrome(0), true);
+    }
+
+    #[test]
+    fn test_123_is_palindrome() {
+        assert_eq!(is_palindrome(123), false);
+    }
+
+    #[test]
+    fn test_121_is_palindrome() {
+        assert_eq!(is_palindrome(121), true);
+    }
+
 }
